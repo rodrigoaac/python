82e1437fe3e1597e122ac13947bf886203c38807
Rodrigo  Aroca Carrera <103465525+rodrigoaac@users.noreply.github.com>
2022-09-15 22:11:54 -0300

    Add files via upload

---
+++ pruebavariables.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+CantidadInvertida = 2500
+
+AnnosRecuperacion =  3
+
+IngresosAnuales = CantidadInvertida / AnnosRecuperacion
+
+
+
+print(IngresosAnuales)
